c6d843708cb50ef4dd6a1a883daae7dcc9433c77
Oliver Gorwits <oliver@cpan.org>
2012-12-19 05:57:01 +0000

    Update test libraries for compatibility with Catalyst 5.81+

--- t/lib/PluginTestApp.pm
+++ t/lib/PluginTestApp.pm
@@ -2,35 +2,10 @@ package PluginTestApp;
 use Test::More;
 
 use Catalyst qw(
-    Loopback
     HashedCookies
 );
 
 __PACKAGE__->config( hashedcookies => { key => 'abcdef0123456789ASDF' } );
 __PACKAGE__->setup;
 
-sub auto : Private {
-    my ( $self, $c ) = @_;
-
-    $c->stash->{'cookies'} = {
-        Catalyst => { value => 'Cool',     path => '/' }, 
-        Cool     => { value => 'Catalyst', path => '/' },
-        CoolCat  => { value => {'Cool' => 'Catalyst'}, path => '/' },
-        BadCat   => { value => {'_hashedcookies_meoww' => 'Catalyst'}, path => '/' },
-    };
-}
-
-sub default : Private {
-    my ( $self, $c ) = @_;
-    my %cookies = %{$c->stash->{'cookies'}};
-
-    # we're testing cookies, here, so this is a little ditty to
-    # set them for us, based on what url path was requested
-    
-    for (split '/', $c->req->path) {
-        $c->log->debug( "$_ => $cookies{ $_ }" ) if $c->debug;
-        if (exists $cookies{ $_ } and defined $cookies{ $_ }) {
-            $c->res->cookies->{ $_ } = $cookies{ $_ };
-        }
-    }
-}
+1;
